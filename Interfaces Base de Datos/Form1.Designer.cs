1c9922a18b627d78fb08b9aa7cef5c251c35b478
JoelDefaz <joe.def2019@gmail.com>
2024-02-18 18:05:27 -0500

    Agregar archivos de proyecto.

---
+++ Interfaces Base de Datos/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Interfaces_Base_de_Datos
+{
+    partial class frmMenú
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnInsertar.Location = new System.Drawing.Point(101, 74);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(183, 63);
+            this.btnInsertar.TabIndex = 0;
+            this.btnInsertar.Text = "Insertar Producto";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnEliminar.Location = new System.Drawing.Point(101, 144);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(183, 63);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar Producto";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnActualizar.Location = new System.Drawing.Point(101, 214);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(183, 63);
+            this.btnActualizar.TabIndex = 2;
+            this.btnActualizar.Text = "Actualizar Producto";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnConsultar.Location = new System.Drawing.Point(101, 284);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(183, 63);
+            this.btnConsultar.TabIndex = 3;
+            this.btnConsultar.Text = "Consultar Productos";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Forte", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RosyBrown;
+            this.label1.Location = new System.Drawing.Point(83, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Menú de Botones";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(316, 316);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(92, 30);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            this.btnSalir.MouseEnter += new System.EventHandler(this.btnSalir_MouseEnter);
+            this.btnSalir.MouseLeave += new System.EventHandler(this.btnSalir_MouseLeave);
+            // 
+            // frmMenú
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 358);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnInsertar);
+            this.Name = "frmMenú";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Interfaces Base de Datos/Form2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaces_Base_de_Datos
+{
+    public partial class frmInsertar : Form
+    {
+        public frmInsertar()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            frmMenú menu = new frmMenú();
+            menu.ShowDialog();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpiarConsola();
+        }
+
+        private void limpiarConsola()
+        {
+            txtCódigo.Text = "";
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+            txtUnidad.Text = "";
+            txtCódigoProveedor.Text = "";
+            txtCódigoTipo.Text = "";
+        }
+
+        string strConn = null;
+        string strComm = null;
+        SqlConnection conn = null;
+        SqlCommand comm = null;
+        string idProd = "";
+        string nombre = "";
+        string precio = "";
+        string unidad = "";
+        string idTipo = "";
+        string idProveedor = "";
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            idProd = txtCódigo.Text;
+            nombre = "'" + txtNombre.Text + "'";
+            precio = txtPrecio.Text;
+            unidad = txtUnidad.Text;
+            idTipo = txtCódigoTipo.Text;
+            idProveedor = txtCódigoProveedor.Text;
+            strConn = "Data Source=(Local); database=Facturación; Integrated Security=SSPI";
+            using (conn = new SqlConnection(strConn))
+            {
+                conn.Open();
+                strComm = "INSERT INTO productos VALUES(" + idProd + "," + nombre + "," + precio + "," + unidad + "," + idTipo + "," + idProveedor + ")";
+                using (comm = new SqlCommand(strComm, conn))
+                {
+                    int num = comm.ExecuteNonQuery();
+                    if (num == 1)
+                    {
+                        MessageBox.Show("Producto ingresado con exito","Mensaje", MessageBoxButtons.OK);
+                    }
+                    limpiarConsola();
+                }
+            }
+        }
+    }
+}
