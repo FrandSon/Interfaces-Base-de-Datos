90c18ce3aa9a7dbfac7c33a6e95e2d91d5f878a1
TuTDem <andreshernandez20001@gmail.com>
2024-02-20 23:01:31 -0500

    Forms 5 y 6, consultar productos

--- Interfaces Base de Datos/Form3.Designer.cs
+++ Interfaces Base de Datos/Form3.Designer.cs
@@ -44,18 +44,20 @@
             // 
             label2.AutoSize = true;
             label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label2.Location = new System.Drawing.Point(30, 58);
+            label2.Location = new System.Drawing.Point(22, 47);
+            label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(484, 20);
+            label2.Size = new System.Drawing.Size(412, 17);
             label2.TabIndex = 41;
             label2.Text = "Seleccione mediante que parámetro desea eliminar un producto";
             // 
             // btnRegresar
             // 
             this.btnRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegresar.Location = new System.Drawing.Point(175, 335);
+            this.btnRegresar.Location = new System.Drawing.Point(131, 272);
+            this.btnRegresar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRegresar.Name = "btnRegresar";
-            this.btnRegresar.Size = new System.Drawing.Size(121, 52);
+            this.btnRegresar.Size = new System.Drawing.Size(91, 42);
             this.btnRegresar.TabIndex = 40;
             this.btnRegresar.Text = "Regresar";
             this.btnRegresar.UseVisualStyleBackColor = true;
@@ -66,9 +68,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Forte", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.RosyBrown;
-            this.label1.Location = new System.Drawing.Point(29, 20);
+            this.label1.Location = new System.Drawing.Point(22, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(225, 27);
+            this.label1.Size = new System.Drawing.Size(182, 22);
             this.label1.TabIndex = 25;
             this.label1.Text = "Eliminar Producto";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -76,9 +79,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(50, 100);
+            this.button1.Location = new System.Drawing.Point(38, 81);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(177, 54);
+            this.button1.Size = new System.Drawing.Size(133, 44);
             this.button1.TabIndex = 42;
             this.button1.Text = "Código";
             this.button1.UseVisualStyleBackColor = true;
@@ -87,9 +91,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(248, 100);
+            this.button2.Location = new System.Drawing.Point(186, 81);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(177, 54);
+            this.button2.Size = new System.Drawing.Size(133, 44);
             this.button2.TabIndex = 43;
             this.button2.Text = "Nombre";
             this.button2.UseVisualStyleBackColor = true;
@@ -98,9 +103,10 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(50, 178);
+            this.button3.Location = new System.Drawing.Point(38, 145);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(177, 54);
+            this.button3.Size = new System.Drawing.Size(133, 44);
             this.button3.TabIndex = 44;
             this.button3.Text = "Precio";
             this.button3.UseVisualStyleBackColor = true;
@@ -109,9 +115,10 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(248, 178);
+            this.button4.Location = new System.Drawing.Point(186, 145);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(177, 54);
+            this.button4.Size = new System.Drawing.Size(133, 44);
             this.button4.TabIndex = 45;
             this.button4.Text = "Unidad";
             this.button4.UseVisualStyleBackColor = true;
@@ -120,9 +127,10 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(50, 256);
+            this.button5.Location = new System.Drawing.Point(38, 208);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(177, 54);
+            this.button5.Size = new System.Drawing.Size(133, 44);
             this.button5.TabIndex = 46;
             this.button5.Text = "Código Proveedor";
             this.button5.UseVisualStyleBackColor = true;
@@ -131,9 +139,10 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(248, 256);
+            this.button6.Location = new System.Drawing.Point(186, 208);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(177, 54);
+            this.button6.Size = new System.Drawing.Size(133, 44);
             this.button6.TabIndex = 47;
             this.button6.Text = "Código Tipo";
             this.button6.UseVisualStyleBackColor = true;
@@ -141,9 +150,9 @@
             // 
             // frmEliminar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(623, 432);
+            this.ClientSize = new System.Drawing.Size(467, 351);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -153,8 +162,10 @@
             this.Controls.Add(label2);
             this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmEliminar";
             this.Text = "Eliminar Producto";
+            this.Load += new System.EventHandler(this.frmEliminar_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Interfaces Base de Datos/Form6.Designer.cs
+++ Interfaces Base de Datos/Form6.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Interfaces_Base_de_Datos
 {
-    partial class frmEliminarConParámetro
+    partial class frmConsultarConParámetro
     {
         /// <summary>
         /// Required designer variable.
@@ -33,8 +33,8 @@
             System.Windows.Forms.Label label4;
             System.Windows.Forms.Label label5;
             this.btnRegresar = new System.Windows.Forms.Button();
-            this.txtParámetroParaEliminar = new System.Windows.Forms.TextBox();
-            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtParámetroParaConsultar = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBoxNumeros = new System.Windows.Forms.ComboBox();
             this.txtParametroNumero = new System.Windows.Forms.TextBox();
@@ -48,7 +48,8 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
             label2.Location = new System.Drawing.Point(36, 84);
             label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             label2.Name = "label2";
@@ -60,7 +61,8 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
             label1.Location = new System.Drawing.Point(36, 186);
             label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             label1.Name = "label1";
@@ -71,74 +73,85 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
             label4.Location = new System.Drawing.Point(21, 60);
             label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(43, 17);
+            label4.Size = new System.Drawing.Size(46, 16);
             label4.TabIndex = 57;
             label4.Text = "Texto";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
             label5.Location = new System.Drawing.Point(21, 160);
             label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new System.Drawing.Size(65, 17);
+            label5.Size = new System.Drawing.Size(69, 16);
             label5.TabIndex = 58;
             label5.Text = "Números";
             // 
             // btnRegresar
             // 
+            this.btnRegresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegresar.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.btnRegresar.Location = new System.Drawing.Point(256, 197);
-            this.btnRegresar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRegresar.Margin = new System.Windows.Forms.Padding(2);
             this.btnRegresar.Name = "btnRegresar";
             this.btnRegresar.Size = new System.Drawing.Size(74, 34);
             this.btnRegresar.TabIndex = 49;
             this.btnRegresar.Text = "Regresar";
-            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.UseVisualStyleBackColor = false;
             this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
             // 
-            // txtParámetroParaEliminar
+            // txtParámetroParaConsultar
             // 
-            this.txtParámetroParaEliminar.Enabled = false;
-            this.txtParámetroParaEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtParámetroParaEliminar.Location = new System.Drawing.Point(39, 113);
-            this.txtParámetroParaEliminar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtParámetroParaEliminar.Name = "txtParámetroParaEliminar";
-            this.txtParámetroParaEliminar.Size = new System.Drawing.Size(169, 24);
-            this.txtParámetroParaEliminar.TabIndex = 48;
-            this.txtParámetroParaEliminar.TextChanged += new System.EventHandler(this.txtParámetroParaEliminar_TextChanged);
+            this.txtParámetroParaConsultar.Enabled = false;
+            this.txtParámetroParaConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtParámetroParaConsultar.Location = new System.Drawing.Point(39, 113);
+            this.txtParámetroParaConsultar.Margin = new System.Windows.Forms.Padding(2);
+            this.txtParámetroParaConsultar.Name = "txtParámetroParaConsultar";
+            this.txtParámetroParaConsultar.Size = new System.Drawing.Size(169, 24);
+            this.txtParámetroParaConsultar.TabIndex = 48;
+            this.txtParámetroParaConsultar.TextChanged += new System.EventHandler(this.txtParámetroParaConsultar_TextChanged);
             // 
-            // btnEliminar
+            // btnConsultar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(256, 124);
-            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(74, 34);
-            this.btnEliminar.TabIndex = 47;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            this.btnConsultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.btnConsultar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConsultar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnConsultar.Location = new System.Drawing.Point(256, 124);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(74, 34);
+            this.btnConsultar.TabIndex = 47;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Forte", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.RosyBrown;
-            this.label3.Location = new System.Drawing.Point(20, 15);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(83, 19);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(182, 22);
+            this.label3.Size = new System.Drawing.Size(188, 24);
             this.label3.TabIndex = 50;
-            this.label3.Text = "Eliminar Producto";
+            this.label3.Text = "Consultar Producto";
             //this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // comboBoxNumeros
             // 
+            this.comboBoxNumeros.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
             this.comboBoxNumeros.Enabled = false;
+            this.comboBoxNumeros.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxNumeros.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.comboBoxNumeros.FormattingEnabled = true;
             this.comboBoxNumeros.Items.AddRange(new object[] {
             "igual",
@@ -147,7 +160,7 @@
             "mayor o igual que",
             "menor o igual que"});
             this.comboBoxNumeros.Location = new System.Drawing.Point(66, 254);
-            this.comboBoxNumeros.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxNumeros.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxNumeros.Name = "comboBoxNumeros";
             this.comboBoxNumeros.Size = new System.Drawing.Size(124, 21);
             this.comboBoxNumeros.TabIndex = 51;
@@ -158,26 +171,30 @@
             this.txtParametroNumero.Enabled = false;
             this.txtParametroNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtParametroNumero.Location = new System.Drawing.Point(39, 217);
-            this.txtParametroNumero.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtParametroNumero.Margin = new System.Windows.Forms.Padding(2);
             this.txtParametroNumero.Name = "txtParametroNumero";
             this.txtParametroNumero.Size = new System.Drawing.Size(169, 24);
             this.txtParametroNumero.TabIndex = 53;
             // 
             // btnCancelar
             // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.btnCancelar.Location = new System.Drawing.Point(256, 160);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(74, 34);
             this.btnCancelar.TabIndex = 56;
             this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.UseVisualStyleBackColor = false;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
-            // frmEliminarConParámetro
+            // frmConsultarConParámetro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
             this.ClientSize = new System.Drawing.Size(355, 334);
             this.Controls.Add(label5);
             this.Controls.Add(label4);
@@ -187,13 +204,13 @@
             this.Controls.Add(this.comboBoxNumeros);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnRegresar);
-            this.Controls.Add(this.txtParámetroParaEliminar);
-            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.txtParámetroParaConsultar);
+            this.Controls.Add(this.btnConsultar);
             this.Controls.Add(label2);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "frmEliminarConParámetro";
-            this.Text = "Eliminar";
-            this.Load += new System.EventHandler(this.frmEliminarConParámetro_Load);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmConsultarConParámetro";
+            this.Text = "Consultar";
+            this.Load += new System.EventHandler(this.frmConsultarConParámetro_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,8 +219,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnRegresar;
-        private System.Windows.Forms.TextBox txtParámetroParaEliminar;
-        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtParámetroParaConsultar;
+        private System.Windows.Forms.Button btnConsultar;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBoxNumeros;
         private System.Windows.Forms.TextBox txtParametroNumero;
